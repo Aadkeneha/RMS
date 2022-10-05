b1effce7bd5896225a5b95b779660ae6b14659ec
Neha Aadke <62541582+Aadkeneha@users.noreply.github.com>
2022-10-05 20:56:05 +0530

    Update views.py

--- new/views.py
+++ new/views.py
@@ -175,6 +175,7 @@ def index(request):
 
 def group_account(request):
     st = "Null"
+    stu={}
     global user_group,rad
     if user_group != "":
         gr = models.Project_Group.objects.filter(grp_number = user_group)
