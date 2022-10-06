a066dbcc8f6b999177250edf3130873c2de37dc3
Neha Aadke <62541582+Aadkeneha@users.noreply.github.com>
2022-10-06 19:47:35 +0530

    commit

--- new/views.py
+++ new/views.py
@@ -241,6 +241,7 @@ def guide_account(request):
 
 def group_dashboard(request):
     st = "Null"
+    stu = {}
     global user_group, rad
     if user_group != "":
         gr = models.Project_Group.objects.filter(grp_number=user_group)
